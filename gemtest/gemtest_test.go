0c105937d5d0e27db62954a29f20b34ae2adba32
askeladdk <askeladdk@users.noreply.github.com>
2023-02-23 13:28:55 +0100

    Add more gemtest tests

--- gemtest/gemtest_test.go
+++ gemtest/gemtest_test.go
@@ -1,15 +1,16 @@
-package gemtest
+package gemtest_test
 
 import (
 	"testing"
 
 	"github.com/askeladdk/gemproto"
+	"github.com/askeladdk/gemproto/gemtest"
 )
 
 func TestServer(t *testing.T) {
 	handler := func(w gemproto.ResponseWriter, r *gemproto.Request) {}
 
-	server := NewServer(gemproto.HandlerFunc(handler))
+	server := gemtest.NewServer(gemproto.HandlerFunc(handler))
 	defer server.Close()
 
 	client := gemproto.Client{}
@@ -21,5 +22,28 @@ func TestServer(t *testing.T) {
 
 	if res.StatusCode != gemproto.StatusOK {
 		t.Fatal(res.StatusCode)
+	}
+}
+
+func TestResponseRecorder(t *testing.T) {
+	h := gemproto.HandlerFunc(func(w gemproto.ResponseWriter, r *gemproto.Request) {
+		w.WriteHeader(gemproto.StatusOK, "text/plain")
+		_, _ = w.Write([]byte("hello world"))
+	})
+
+	w := gemtest.NewRecorder()
+	r := gemtest.NewRequest("/")
+	h.ServeGemini(w, r)
+
+	if w.Code != gemproto.StatusOK {
+		t.Error(w.Code)
+	}
+
+	if w.Meta != "text/plain" {
+		t.Error(w.Meta)
+	}
+
+	if s := w.Body.String(); s != "hello world" {
+		t.Error(s)
 	}
 }
